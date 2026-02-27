5961542071d65211a741546fc0947b0e4039812b
Abo Obada <abooyaman999@gmail.com>
2026-02-27 05:46:39 +0300

    final

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,29 +4,39 @@ import { ThemeContext } from "../contexts/ThemeContext"
 export default function About(){
     const theme = useContext(ThemeContext);
 return(
-<div className="flex-col flex m-20 snap-y snap-mandatory">
- <div className="fadeup-animation ">
+<div className="flex flex-col m-20 h-screen overflow-y-scroll snap-y snap-mandatory ">
+ <section className="snap-start">
+    <div className="fadeup-animation ">
     <h1 className="text-[7rem] ">من نحن؟</h1>
  </div>
   <div>
     <h1 className="text-[5rem] place-self-end-safe fadeup-animation">نحن فريق عمل صغير يعمل على مراجعة الكتب وفقاً لفهم فكرة أو  إيديولوجية الراوي أو الكاتب و أحيانا ننشر الأحاديث و القرأن الكريم وفقا لفغهم المفسرين أيضاً</h1>
  </div>
+ </section>
 
- <div className="self-end mt-40 fadeup-animation">
+<section className="snap-start">
+     <div className="self-end mt-40 fadeup-animation">
     <h1 className="text-[7rem] ">ما هي غايتنا؟</h1>
  </div>
   <div>
     <h1 className="text-[5rem] place-self-end-safe fadeup-animation"> غايتنا هي إدلاء الرأي بشكل صريح بدون تلميع أو شيطنة الكتاب أو صاحب الراوي على إثر معتقده أو فكره.نحن لا نتبنى فكر أي كاتب أو قرائ, بل نشرحها</h1>
  </div>
+</section>
 
- <div className="self-start mt-40 fadeup-animation">
+<section className="flex flex-col snap-start">
+     <div className="self-start mt-40 fadeup-animation">
     <h1 className="text-[7rem] fadeup-animation">من هو مالك صفحة حفار؟</h1>
  </div>
   <div>
     <h1 className="text-[5rem] place-self-end-safe fadeup-animation"> صفحة حفار يمتلكها "صلاح حفار" بشكل رسمي: صلاح حفار هو من مواليد سوريا, درس تخصص تعويضات السنية في الجامعة روملة التركية</h1>
  </div>
 
- <div className="self-center mt-50 fadeup-animation">
+</section>
+
+ <section className="flex justify-center snap-start">
+    <div className="self-center mt-50 fadeup-animation">
     <img src={Logo} alt="" className={`${theme && theme.theme == "dark"?  'invert' :'' }`}/>
  </div>
+ </section>
+
 </div>)}
